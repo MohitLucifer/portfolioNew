9bb549080092414858410830e271d670b28a5455
MOHIT KUMAR <mohit7282kumar@gmail.com>
2024-05-19 08:40:30 +0530

    project added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -28,18 +28,18 @@ function Projects() {
               title="StoreSavvy ( React.Js,Node.Js,MongoDB ,HTML,CSS,Javascript)"
               description=" An E-Commerce project developed using modern frontend technologies.CRUD operations is used.Functionality of view,search,filter product,Admin panel,User Panel,Payment,Add-to-cart option,JWT authentication.Admins have access to monitor the payments ,orders and users via Dashboard"
               ghLink="https://github.com/MohitLucifer/ECOMMERCE_HOST.git"
-              demoLink="https://shiny-gold-tank-top.cyclic.app/"
+              demoLink="https://ecommerce-host.onrender.com/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={snappy}
               isBlog={false}
-              title="Chat-Room"
+              title="SnappyTalk"
               description="It is a app where any person can join and chat with other person which is also present on it.It has real-time chatting system.Using Node.js and Socket.IO, a chat room fosters real-time, multi-user interaction on the web, enabling seamless communication and message exchange among participants across various devices and locations."
-              ghLink="https://github.com/MohitLucifer/chatSocket"
-              // demoLink="https://blogs.soumya-jit.tech/"
+              ghLink="https://github.com/MohitLucifer/chatAppHost"
+              demoLink="https://snappytalk.netlify.app/login"
             />
           </Col>
 
@@ -82,6 +82,16 @@ function Projects() {
               title="BuzzBit (React Native,API-Fetch,Node.Js,HTML,CSS)"
               description="React Native, JavaScript, CSS for front-end development.API fetching to retrieve and display live news updates.Introduced multiple tabs (Discover, News, Profile, Saved)in BuzzBit, offering trending news, easy segment access, user profile management, and article bookmarking for enhanced content discoverability and user convenience"
               ghLink="https://github.com/MohitLucifer/React-Native-NewsApp"
+              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={face}
+              isBlog={false}
+              title="Face Attendence Tracker"
+              description="The face attendance tracker is an AI-based system that uses facial recognition to mark attendance by identifying and verifying student identities from webcam images. It integrates with a Firebase database to manage attendance records and leverages computer vision techniques to process and recognize faces in real-time."
+              ghLink="https://github.com/MohitLucifer/Face-recognition_attendenec"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
